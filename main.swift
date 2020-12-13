231fb4c2a1f69eff004f7d7280a46069759cc7d1
Roger Aquino <kensukeaida00@gmail.com>
2020-12-13 20:04:21 +0000

    Añadir la clase ejercicio 3

--- main.swift
+++ main.swift
@@ -2,6 +2,7 @@
 //Para la clase de Computo movil
 //Fecha: 13/12/2020
 import Glibc
+
 class ejercicioUno{
   let base:Int
   var res:Int
@@ -10,7 +11,10 @@ class ejercicioUno{
   init(){
     self.base=2
     self.res=2
-    self.n=1
+    print("Ingrese numero entero para realizar la operacion 2^n",
+          "\nN=")
+    let n=readLine()!
+    self.n=Int(n)!
   }
   func upperNum(n:Int){
     if n<=1{
@@ -23,15 +27,12 @@ class ejercicioUno{
     return(upperNum(n:n-1))
   }
   func prepUpper(){
-    print("Ingrese numero entero para realizar la operacion 2^n",
-          "\nN=")
-    let n=readLine()!
-    self.n=Int(n)!
     upperNum(n:self.n)
     print("Resultado de 2^n=",self.res)
     return
   }
 }
+
 class ejercicioDos{
   let a:Int
   let b:Int
@@ -60,12 +61,70 @@ class ejercicioDos{
     return
   }
 }
-func main(){
-  let ejuno=ejercicioUno()
-  ejuno.prepUpper()
-  let ejdos=ejercicioDos()
-  ejdos.resSuma()
+
+class ejercicioTres{
+  let numero:String
+  let numArray:[Character]
+
+  init(){
+    print("Ingrese el numero de telefono con formato (nn)-nnnn-nnnn",
+    "\nNumero:")
+    self.numero=readLine()!
+    self.numArray=Array(numero)
+  }
+
+  func validaNum()->Bool{
+    var i=0
+    while i<14{
+      if i != 4&&i != 9&&i != 0&&i != 3{
+        switch self.numArray[i]{
+          case "0"..."9":
+          break
+          default:
+          print("Error, no es un numero valido")
+          return false
+        }
+      }
+      i=i+1
+    }
+    return true
+  }
+  func validaExtras()->Bool{
+    if self.numArray.count != 14{
+      print("Tamaño incorrecto")
+      return false
+    }
+    if self.numArray[4] != "-" || self.numArray[9] != "-"{
+      print("Faltan el/los '-'")
+      return false
+    }
+    if self.numArray[0] != "(" || self.numArray[3] != ")"{
+      print("Falta(n) el/los '()'")
+      return false
+    }
+    return validaNum()
+  }
+  func validador(){
+    if validaExtras()==true{
+      print("El numero es valido")
+      return
+    }
+    else{
+      print("El numero no es valido")
+    }
+    return
+  }
 }
-//let n=pow(Double(2),Double(-3))
-//print(n)
+
+class ejercicioCuatro{
+  
+}
+func main(){
+  //let ejuno=ejercicioUno()
+  //ejuno.prepUpper()
+  //let ejdos=ejercicioDos()
+  //ejdos.resSuma()
+  let ejtres=ejercicioTres()
+  ejtres.validador()
+}
 main()
